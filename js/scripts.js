72015fa89d39bd9ec29e06f1255d7d221171ca2e
Tarek Safa <tarek1000100@gmail.com>
2018-11-15 09:45:40 -0500

    Revert "query attempt"
    This reverts commit a71277b4c3c93b9d6355c00bc791c173ddf3a8ac.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,3 @@
 //write your javascript here!
 
 console.log('Starting CityView...')
-
-function loadData() {
-  var data = {
-    resource_id: 'c53f0204-3b39-4a33-8068-64168dbe9847', // the resource id
-    limit: 5, // get 5 results
-    q: 'jones' // query for 'jones'
-  };
-  $.ajax({
-    url: 'https://data.boston.gov/api/3/action/datastore_search',
-    data: data,
-    dataType: 'jsonp',
-    success: function(data) {
-      alert('Total results found: ' + data.result.total)
-    }
-  });
-
-  
-}
