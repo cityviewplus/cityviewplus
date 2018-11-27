43153101923309cb65ae8a8c6d33a2d8928ddd54
Erick Moo <zmoofeng@wpi.edu>
2018-11-27 13:15:06 -0500

    Updated data querying and processing functions
    - Made data querying and processing functions more usable
    - Added comments to data processing functions
    - Started work on sunburst diagram

--- js/dataset_processing.js
+++ js/dataset_processing.js
@@ -1,16 +1,18 @@
 /*
- * Queries a specified dataset from Analyze Boston
+ * Queries a specified dataset from Analyze Boston or a specified url
  * @param {string} query - Query string (resource id = table name, use template string to wrap the query,
  * use double quotes to wrap the table names, and use single quotes to wrap the column names)
+ * @param {string} url - optional url to query data from another source
  * @return {Json} dataJsonArray - preprocessed array of Json objects from the query and preprocessData()
  */
-function getData(query, callback) {
+function getData(query, callback, url) {
+  url = url || 'https://data.boston.gov/api/3/action/datastore_search_sql?sql=';
   var rawJson, dataJson;
   var request = new XMLHttpRequest();
   request.responseType = 'text';
   request.onreadystatechange = function() {
     if (request.readyState === 4 && request.status === 200) {
-      rawJson = Json.parse(request.responseText);
+      rawJson = JSON.parse(request.responseText);
       console.log(rawJson);
       dataJsonArray = preprocessData(rawJson);
 
@@ -20,7 +22,7 @@ function getData(query, callback) {
     }
   };
 
-  request.open('GET', 'https://data.boston.gov/api/3/action/datastore_search_sql?sql=' + query);
+  request.open('GET', url + query);
   request.send();
 }
 
@@ -42,15 +44,42 @@ function preprocessData(rawJson) {
 
 }
 
-//TODO make function to map JsonArray
-function mapJsonArray(dataJsonArray, keyCol, valueCol, key, value) {
-  key = key || 'name';
-  value = value || 'size';
+/*
+ * Maps data in a Json object array to two key-value pairs (one value containing categorical data,
+ * and the other containing numerical data)
+ * @param {Json array} dataJsonArray - array of Json objects containing key-value pairs to be mapped
+ * @param {string} catCol - column name containing categorical data
+ * @param {string} numCol - column name containing numerical data
+ * @param {string} catKey - optional parameter to name the key of the categorical data
+ * @param {string} numKey - optional parameter to name the key of the numerical data
+ * @return {Json array} mapped - array of Json objects contining the mapped key-value pairs
+*/
+function mapJsonArray(dataJsonArray, catCol, numCol, catKey, numKey) {
+  catKey = catKey || 'name';
+  numKey = numKey || 'size';
   var mapped = dataJsonArray.map(function(d, i) {
     return {
-      [key]: d[keyCol],
-      [value]: d[valueCol]
+      [catKey]: d[catCol],
+      [numKey]: d[numCol]
     };
   });
   return mapped;
+}
+
+/*
+ * Creates a new Json object with a Json array as its child in a key-value pairs
+ * @param {Json array} dataJsonArray -
+ * @param {string} name - a descriptive name that describes the data that the child Json array contains
+ * @param {string} nameKey - optional string for the name key
+ * @param {string} childKey - optional string for the child key
+ * @return {Json} json - new Json object
+*/
+function addChildToJson(dataJsonArray, name, nameKey, childKey) {
+  nameKey = nameKey || 'name';
+  childKey = childKey || 'children';
+  var json = {};
+  json[nameKey] = name;
+  json[childKey] = dataJsonArray;
+
+  return json;
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,91 @@
 //write your javascript here!
 
-console.log('Starting CityView...')
+console.log('Starting CityView...');
 
 var read = `SELECT * FROM "20f64c02-6023-4280-8131-e8c0cedcae9b" LIMIT 100`;
 
 var data = getData(read, function(jsonArray) {
   var ul = document.getElementById('jsonList');
-  var dataJson = mapJsonArray(json, 'Neighborhood', 'Percent of Population');
+  var dataJson = addChildToJson(
+    mapJsonArray(jsonArray, 'Neighborhood', 'Percent of Population'), 'Neighborhood');
+
+  console.log(dataJson);
 
   //https://stackoverflow.com/questions/921789/how-to-loop-through-a-plain-javascript-object-with-the-objects-as-members
-  for (var key in dataJson) {
-    // skip loop if the property is from prototype
-    if (!dataJson.hasOwnProperty(key)) continue;
-
-    var obj = dataJson[key];
-    for (var prop in obj) {
-        // skip loop if the property is from prototype
-        if(!obj.hasOwnProperty(prop)) continue;
-
-        var node = document.createElement("LI");
-        var textnode = document.createTextNode(prop + " = " + obj[prop]);
-        node.appendChild(textnode);
-        ul.appendChild(node);
-    }
-
-    ul.innerHTML = ul.innerHTML + "<br/>" + "--------------------------" + "<br/>";
-}
+  // for (var key in dataJson) {
+  //   // skip loop if the property is from prototype
+  //   if (!dataJson.hasOwnProperty(key)) continue;
+  //
+  //   var obj = dataJson[key];
+  //   for (var prop in obj) {
+  //     // skip loop if the property is from prototype
+  //     if (!obj.hasOwnProperty(prop)) continue;
+  //
+  //     var node = document.createElement("LI");
+  //     var textnode = document.createTextNode(prop + " = " + obj[prop]);
+  //     node.appendChild(textnode);
+  //     ul.appendChild(node);
+  //   }
+  //
+  //   ul.innerHTML = ul.innerHTML + "--------------------------" + "<br/>";
+  // }
 });
 
+var width = 960,
+    height = 700,
+    radius = (Math.min(width, height) / 2) - 10;
+
+var formatNumber = d3.format(",d");
+
+var x = d3.scaleLinear()
+    .range([0, 2 * Math.PI]);
+
+var y = d3.scaleSqrt()
+    .range([0, radius]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var partition = d3.partition();
+
+var arc = d3.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+d3.json("https://gist.githubusercontent.com/mbostock/4348373/raw/85f18ac90409caa5529b32156aa6e71cf985263f/flare.json", function(error, root) {
+  if (error) throw error;
+
+  root = d3.hierarchy(root);
+  root.sum(function(d) { return d.size; });
+  svg.selectAll("path")
+      .data(partition(root).descendants())
+    .enter().append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+      .on("click", click)
+    .append("title")
+      .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
+});
+
+function click(d) {
+  svg.transition()
+      .duration(750)
+      .tween("scale", function() {
+        var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+            yd = d3.interpolate(y.domain(), [d.y0, 1]),
+            yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
+        return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+      })
+    .selectAll("path")
+      .attrTween("d", function(d) { return function() { return arc(d); }; });
+}
+
+d3.select(self.frameElement).style("height", height + "px");
