1c858962c36fa73103319177a0cdc1910beaec9b
Unknown <zmoofeng@wpi.edu>
2018-12-12 17:34:20 -0500

    Integrate Leaflet map to Events page #12
    - Added Leaflet map to events page with layers showing Crimes and Emergency Snow Parking markers.

--- js/events_leaflet.js
+++ js/events_leaflet.js
@@ -1,7 +1,8 @@
-// edit read query
-//var query = `SELECT * FROM "20f64c02-6023-4280-8131-e8c0cedcae9b"`;
 var query = `SELECT * FROM "12cb3883-56f5-47de-afa5-3b1cf61b257b" WHERE "YEAR" = '2018'
 AND CAST("MONTH" AS INT) = 12`;
+
+var showingCrimes = false;
+var parkingLayer = null;
 
 var crimesCluster = L.markerClusterGroup({
   // iconCreateFunction is used if we are not using MarkerCluster.Default.css
@@ -25,25 +26,104 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: 'pk.eyJ1IjoiZXJpY2ttb28iLCJhIjoiY2pwMW9zdnplMDB0MDNrcGV3Y3dpdDdxYyJ9.PKvFcq8kWpz8bH3G2taIjA'
 }).addTo(map);
 
-getData(query, function(jsonArray) {
-  var dataLength = jsonArray.length;
-  var latList = [];
-  var lonList = [];
-  // iterate over all data points
-  for (var i = 0; i < dataLength; i++) {
-    latList.push(Number(jsonArray[i].Lat));
-    lonList.push(Number(jsonArray[i].Long));
+loadCrimesLayer();
+loadParkingLayer();
+
+function loadCrimesLayer() {
+  getData(query, function(jsonArray) {
+    var dataLength = jsonArray.length;
+    var latList = [];
+    var lonList = [];
+    var incidentList = [];
+
+    // iterate over all data points
+    for (var i = 0; i < dataLength; i++) {
+      latList.push(Number(jsonArray[i].Lat));
+      lonList.push(Number(jsonArray[i].Long));
+      incidentList.push(jsonArray[i].OFFENSE_CODE_GROUP);
+    }
+
+    // Add markers to cluster group
+    var crimesMarkerList = [];
+    for (var i = 0; i < dataLength; i++) {
+      var marker = L.circleMarker(getLatLon(i, latList, lonList), {
+        radius: 5,
+        color: 'red'
+      }).bindPopup(incidentList[i]);
+      crimesMarkerList.push(marker);
+    }
+
+    crimesCluster.addLayers(crimesMarkerList);
+    showCrimes();
+  });
+}
+
+function loadParkingLayer() {
+  var request = new XMLHttpRequest();
+  request.responseType = 'text';
+  request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+      geoJsonData = JSON.parse(request.responseText);
+      console.log(geoJsonData);
+
+      parkingLayer = L.geoJSON(geoJsonData, {
+        pointToLayer: function(point, latlng) {
+          return L.circleMarker(latlng, {
+            radius: 5
+          });
+        }
+      })
+      .bindPopup(function(layer) {
+        //return layer.feature.properties.Address;
+        return `Name: ${layer.feature.properties.Name}<br>
+        Phone: ${layer.feature.properties.Phone}<br>
+        Spaces: ${layer.feature.properties.Spaces}<br>
+        Fee: ${layer.feature.properties.Fee}<br>
+        Comments: ${layer.feature.properties.Comments}`;
+      });
+    }
+  };
+
+  request.open('GET', 'datasets/Snow_Emergency_Parking.geojson');
+  request.send();
+}
+
+function showCrimes() {
+  if (showingCrimes) {
+    console.log("The map is already showing crimes")
+    return;
   }
 
-  // Add markers to cluster group
-  var treesMarkerList = [];
-  for (var i = 0; i < dataLength; i++) {
-    var marker = L.circleMarker(getLatLon(i, latList, lonList), {
-      radius: 1,
-      color: 'green'
-    });
-    treesMarkerList.push(marker);
+  if (parkingLayer != null) {
+    console.log("removing parking layer")
+    parkingLayer.remove();
   }
 
-  treesCluster.addLayers(treesMarkerList);
-});
+  crimesCluster.addTo(map);
+
+  showingCrimes = true;
+}
+
+function showParking() {
+  if (!showingCrimes) {
+    console.log("The map is already showing snow emergency parking")
+    return;
+  }
+
+  if (crimesCluster != null) {
+    console.log("removing crime layer")
+    crimesCluster.remove();
+  }
+
+  parkingLayer.addTo(map);
+
+  showingCrimes = false;
+}
+
+// HELPER: Get (lat,lon) for specific point
+function getLatLon(i, latList, lonList) {
+  return [
+    latList[i],
+    lonList[i]
+  ];
+};
