2f1da816cdd4f184305ccc343b9e780c2caaab94
Tarek Safa <tarek1000100@gmail.com>
2019-02-03 18:39:41 -0500

    update
    changed colors to be highlight races, work in progress

--- js/sunburst.js
+++ js/sunburst.js
@@ -2,35 +2,64 @@ var query = `SELECT * FROM "20f64c02-6023-4280-8131-e8c0cedcae9b"`;
 
 getData(query, function(jsonArray) {
 
+  var height = 750;
+  var width = 750;
+  var radius = width / 8;
+
+  margin = ({
+    top: 20,
+    right: 0,
+    bottom: 30,
+    left: 40
+  })
+
   var dataJson = d3.nest()
-  .key(function (d) {return d.Decade;})
-  .key(function (d) {return d.Neighborhood;})
-  .key(function (d) {return d["Race and or Ethnicity"];})
-  .rollup(function (v) {return parseInt(v[0]["Percent of Population"])})
-  .entries(jsonArray);
+    .key(function(d) {
+      return d.Decade;
+    })
+    .key(function(d) {
+      return d.Neighborhood;
+    })
+    .key(function(d) {
+      return d["Race and or Ethnicity"];
+    })
+    .rollup(function(v) {
+      return parseFloat(v[0]["Percent of Population"])
+    })
+    .entries(jsonArray);
 
   dataJson = addChildToJson(dataJson, "Race by decade", "key", "values");
 
   function renameKeys(d) {
-    d.name = d.key; delete d.key;
+    d.name = d.key;
+    delete d.key;
     if (typeof d.value === "number") d.size = d.value;
     else d.values.forEach(renameKeys), d.children = d.values;
     delete d.values;
   }
 
+
   renameKeys(dataJson);
 
-  var height = 1000;
-  var width = 1000;
-  var radius = width / 8;
+  console.log("3" + JSON.stringify(jsonArray))
+
 
   var format = d3.format(",d");
 
   var color = d3.scaleOrdinal().range(d3.quantize(d3.interpolateRainbow, dataJson.children.length + 1));
+  var purple = d3.scaleOrdinal().domain(dataJson.children.length + 1).range(['rgb(110, 64, 170)']);
+  var pink = d3.scaleOrdinal().domain(dataJson.children.length + 1).range(['rgb(223, 64, 161)']);
+  var orange = d3.scaleOrdinal().domain(dataJson.children.length + 1).range(['rgb(255, 112, 78)']);
+  var yellow = d3.scaleOrdinal().domain(dataJson.children.length + 1).range(['rgb(210, 201, 52)']);
+  var green = d3.scaleOrdinal().domain(dataJson.children.length + 1).range(['rgb(107, 247, 92)']);
+  var turqouise = d3.scaleOrdinal().domain(dataJson.children.length + 1).range(['rgb(27, 217, 172)']);
+  var blue = d3.scaleOrdinal().domain(dataJson.children.length + 1).range(['rgb(57, 136, 225)'])
 
   var partition = data => {
     const root = d3.hierarchy(data)
-      .sum(d => { return d.size; })
+      .sum(d => {
+        return d.size;
+      })
       .sort((a, b) => b.value - a.value);
     return d3.partition()
       .size([2 * Math.PI, root.height + 1])
@@ -45,16 +74,66 @@ getData(query, function(jsonArray) {
     .innerRadius(d => d.y0 * radius)
     .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1));
 
+  var chart2 = () => {
+    const root = partition(dataJson);
+
+    root.each(d => d.current = d);
+
+    // Variables for chart
+
+    x = d3.scaleBand()
+      .domain(d => d.data.name)
+      .range([margin.left, width])
+      .padding(0.1);
+
+    y = d3.scaleLinear()
+      .domain([0, d => d.value]).nice()
+      .range([height]);
+
+    xAxis = g => g
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    yAxis = g => g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .call(g => g.select(".domain").remove())
+
+    var svg = d3.select("#chart").append("svg")
+      .attr("width", width)
+      .attr("height", "100%")
+      //.attr("style", "margin-left:auto; margin-right:auto; display:block;")
+      .attr("style", "margin-left:auto; float:right; display:block;")
+      .style("font", "10px sans-serif");
+
+    const g = svg.append("g")
+      .attr("transform", `translate(${width / 2},${height / 2.05})`)
+      .attr("fill", "steelblue")
+      .selectAll("rect")
+      .data(root)
+      .attr("x", d => x(d.data.name))
+      .attr("y", d => y(d.value))
+      .attr("height", height)
+      .attr("width", width);
+
+    g.append("g")
+      .call(xAxis)
+      .call(yAxis);
+
+    return svg.node();
+
+  }
+
   var chart = () => {
     const root = partition(dataJson);
 
     root.each(d => d.current = d);
 
     var svg = d3.select("#chart").append("svg")
-        .attr("width", width)
-        .attr("height", "100%")
-        .attr("style", "margin-left:auto; margin-right:auto; display:block;")
-        .style("font", "10px sans-serif");
+      .attr("width", width)
+      .attr("height", "100%")
+      .attr("style", "margin-left:auto; margin-right:auto; display:block;")
+      .style("font", "10px sans-serif");
 
     const g = svg.append("g")
       .attr("transform", `translate(${width / 2},${height / 2.05})`);
@@ -64,18 +143,43 @@ getData(query, function(jsonArray) {
       .data(root.descendants().slice(1))
       .enter().append("path")
       .attr("fill", d => {
-        while (d.depth > 1) d = d.parent;
-        return color(d.data.name);
+        //while (d.depth > 2) {
+        //d = d.parent;
+        //}
+        if (d.depth == 3) {
+          console.log("got white")
+          return color(d.data.name);
+        } else if (d.depth == 2 && d.parent.data.name == "1950") {
+          return yellow(d.data.name);
+        } else if (d.depth == 2 && d.parent.data.name == "1960") {
+          return purple(d.data.name);
+        } else if (d.depth == 2 && d.parent.data.name == "1970") {
+          return blue(d.data.name);
+        } else if (d.depth == 2 && d.parent.data.name == "1980") {
+          return green(d.data.name);
+        } else if (d.depth == 2 && d.parent.data.name == "1990") {
+          return pink(d.data.name);
+        } else if (d.depth == 2 && d.parent.data.name == "2000") {
+          return turqouise(d.data.name);
+        } else if (d.depth == 2 && d.parent.data.name == "2010") {
+          return orange(d.data.name);
+        } else {
+          console.log("got something else")
+          return color(d.data.name);
+        }
       })
       .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
       .attr("d", d => arc(d.current));
 
     path.filter(d => d.children)
-    .style("cursor", "pointer")
-    .on("click", clicked);
+      .style("cursor", "pointer")
+      .on("click", clicked); //d => {if(d.children) {
+    //clicked(d.current)
+    //} else {clicked(d.current)}});
+
 
     path.append("title")
-    .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+      .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
 
     const label = g.append("g")
       .attr("pointer-events", "none")
@@ -87,7 +191,7 @@ getData(query, function(jsonArray) {
       .attr("dy", "0.35em")
       .attr("fill-opacity", d => +labelVisible(d.current))
       .attr("transform", d => labelTransform(d.current))
-      .text(d => d.data.name);
+      .text(d => d.children ? `${d.data.name}` : `${d.data.name} ${d.value}%`) //+ arc.startAngle(d.current));
 
     const parent = g.append("circle")
       .datum(root)
@@ -96,37 +200,107 @@ getData(query, function(jsonArray) {
       .attr("pointer-events", "all")
       .on("click", clicked);
 
+
     function clicked(p) {
+
+      console.log(p)
+
       parent.datum(p.parent || root);
 
-      root.each(d => d.target = {
-        x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-        x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-        y0: Math.max(0, d.y0 - p.depth),
-        y1: Math.max(0, d.y1 - p.depth)
-      });
+      if (p.depth == 2) {
 
-      const t = g.transition().duration(750);
+        var childrenList = []
 
-      // Transition the data on all arcs, even the ones that aren’t visible,
-      // so that if this transition is interrupted, entering arcs will start
-      // the next transition from the desired position.
-      path.transition(t)
-        .tween("data", d => {
-          const i = d3.interpolate(d.current, d.target);
-          return t => d.current = i(t);
-        })
-        .filter(function(d) {
-          return +this.getAttribute("fill-opacity") || arcVisible(d.target);
-        })
-        .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
-        .attrTween("d", d => () => arc(d.current));
+        for(i = 0 ; i < 5 ; i++) {
+          childrenList.push(p.children[i].data.name);
+        }
 
-      label.filter(function(d) {
-          return +this.getAttribute("fill-opacity") || labelVisible(d.target);
-        }).transition(t)
-        .attr("fill-opacity", d => +labelVisible(d.target))
-        .attrTween("transform", d => () => labelTransform(d.current));
+        const t = g.transition().duration(750);
+
+        // Transition the data on all arcs, even the ones that aren’t visible,
+        // so that if this transition is interrupted, entering arcs will start
+        // the next transition from the desired position.
+        path.transition(t)
+          .tween("data", d => {
+            const i = d3.interpolate(d.current, d.target);
+            return t => d.current = i(t);
+          })
+          .filter(function(d) {
+            return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+          })
+          .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+          .attrTween("d", d => () => arc(d.current));
+
+        label.filter(function(d) {
+            return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+          }).transition(t)
+          .attr("fill-opacity", d => +labelVisible(d.target))
+          .attrTween("transform", d => () => labelTransform(d.current));
+
+        //const svg = d3.select(DOM.svg(width, height));
+        x = d3.scaleBand()
+          .domain(childrenList)
+          .range([margin.left, width - margin.right])
+          .padding(0.1)
+
+        y = d3.scaleLinear()
+          .domain([0, p.value]).nice()
+          .range([height - margin.bottom, margin.top])
+
+        xAxis = g => g
+          .attr("transform", `translate(0,${height - margin.bottom})`)
+          .call(d3.axisBottom(x).tickSizeOuter(0))
+
+        yAxis = g => g
+          .attr("transform", `translate(${margin.left},0)`)
+          .call(d3.axisLeft(y))
+          .call(g => g.select(".domain").remove())
+
+        svg.append("g")
+          .attr("fill", "steelblue")
+          .selectAll("rect")
+          .attr("x", x(p.data.name))
+          .attr("y", y(p.value))
+          .attr("height", height)
+          .attr("width", x.bandwidth());
+
+        svg.append("g")
+          .call(xAxis);
+
+        svg.append("g")
+          .call(yAxis);
+
+      } else {
+
+        root.each(d => d.target = {
+          x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+          x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+          y0: Math.max(0, d.y0 - p.depth),
+          y1: Math.max(0, d.y1 - p.depth)
+        });
+
+        const t = g.transition().duration(750);
+
+        // Transition the data on all arcs, even the ones that aren’t visible,
+        // so that if this transition is interrupted, entering arcs will start
+        // the next transition from the desired position.
+        path.transition(t)
+          .tween("data", d => {
+            const i = d3.interpolate(d.current, d.target);
+            return t => d.current = i(t);
+          })
+          .filter(function(d) {
+            return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+          })
+          .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+          .attrTween("d", d => () => arc(d.current));
+
+        label.filter(function(d) {
+            return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+          }).transition(t)
+          .attr("fill-opacity", d => +labelVisible(d.target))
+          .attrTween("transform", d => () => labelTransform(d.current));
+      }
     }
 
     function arcVisible(d) {
@@ -147,4 +321,5 @@ getData(query, function(jsonArray) {
   };
 
   chart();
+  //chart2();
 });
